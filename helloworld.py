dd5a77ddb16a10f180c267cdf186c7fff550f9e9
kgaba <kgaba@umich.edu>
2019-09-09 15:44:15 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import random
+
+print("Kanu")
+
+rand1 = random.randint(0,100)
+print(rand1)
+rand2 = random.randint(0,100)
+print(rand2)
+
+sum = rand1 + rand2
+print("Sum =", sum)
+
+avg = sum / 2.0
+print("Average =", avg)
